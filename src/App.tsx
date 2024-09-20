a22e163f22f8e480809199ac62842f3cb023a65c
Daniel Vidaillet Rojas <dvidaillet90@gmail.com>
2024-09-20 12:42:13 -0500

    add app container

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useTranslation } from "react-i18next";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import "./i18n"; // Importar configuración de i18n
-import Navbar from "./components/Navbar/Navbar";
-import ThemeToggleButton from "./components/buttons/ThemeToggleButton/ThemeToggleButton";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import Home from './pages/Home';
+import About from './pages/About';
+import Navbar from './components/Navbar/Navbar';
+import ThemeToggleButton from './components/buttons/ThemeToggleButton/ThemeToggleButton';
+import './i18n'; // Importar configuración de i18n
+import './App.css'; // Importar estilos globales
 
-const App: React.FC = () => {
+const App = () => {
   const { i18n } = useTranslation();
 
   const changeLanguage = (lang: string) => {
@@ -15,22 +15,22 @@ const App: React.FC = () => {
   };
 
   return (
-    <>
-    <header>
-      <ThemeToggleButton />
-    </header>
+    <div className="app-container">
+      <header>
+        <ThemeToggleButton />
+      </header>
       <Router>
         <Navbar />
-        <div style={{ padding: "20px" }}>
-          <button onClick={() => changeLanguage("en")}>English</button>
-          <button onClick={() => changeLanguage("es")}>Español</button>
+        <div style={{ padding: '20px' }}>
+          <button onClick={() => changeLanguage('en')}>English</button>
+          <button onClick={() => changeLanguage('es')}>Español</button>
         </div>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
         </Routes>
       </Router>
-    </>
+    </div>
   );
 };
 
