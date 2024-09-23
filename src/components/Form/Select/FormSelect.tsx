1b62da8f3ca7b646bee4ef4adc3940052bce8ef6
Daniel Vidaillet Rojas <dvidaillet90@gmail.com>
2024-09-23 13:11:54 -0500

    improving select form field

--- src/components/Form/Select/FormSelect.tsx
+++ src/components/Form/Select/FormSelect.tsx
@@ -6,7 +6,10 @@ import {
   FormControl,
   InputLabel,
   FormHelperText,
+  Chip,
+  Box,
 } from "@mui/material";
+import { OutlinedInput } from "@mui/material";
 
 interface FormSelectProps {
   name: string;
@@ -15,6 +18,7 @@ interface FormSelectProps {
   label: string;
   options: { value: string; label: string }[];
   errorMessage?: string;
+  multiple?: boolean; // Para seleccionar múltiples opciones
 }
 
 const FormSelectComponent: React.FC<FormSelectProps> = ({
@@ -23,6 +27,7 @@ const FormSelectComponent: React.FC<FormSelectProps> = ({
   label,
   options,
   errorMessage,
+  multiple = false,
 }) => {
   return (
     <Controller
@@ -31,7 +36,39 @@ const FormSelectComponent: React.FC<FormSelectProps> = ({
       render={({ field }) => (
         <FormControl fullWidth error={!!errorMessage}>
           <InputLabel>{label}</InputLabel>
-          <Select {...field} label={label}>
+          <Select
+            {...field}
+            label={label}
+            multiple={multiple}
+            input={<OutlinedInput label={label} />}
+            renderValue={(selected) => {
+              if (Array.isArray(selected)) {
+                return (
+                  <Box sx={{ display: "flex", flexWrap: "wrap" }}>
+                    {selected.map((value) => (
+                      <Chip
+                        key={value}
+                        label={
+                          options.find((option) => option.value === value)
+                            ?.label
+                        }
+                        sx={{ m: 0.5 }}
+                      />
+                    ))}
+                  </Box>
+                );
+              }
+              return selected;
+            }}
+            MenuProps={{
+              PaperProps: {
+                style: {
+                  maxHeight: 48 * 4.5 + 8, // Ajusta la altura del menú
+                  width: 250,
+                },
+              },
+            }}
+          >
             {options.map((option) => (
               <MenuItem key={option.value} value={option.value}>
                 {option.label}
